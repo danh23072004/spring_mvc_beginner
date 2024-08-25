ec517f0f6e6f52266f37e2f7b134ccfba2a4b805
danh55162 <danhcoder23072004@gmail.com>
2024-08-25 08:31:33 +0700

    Configure project for beginner

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,16 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        // currentContext is treated as a container for all the beans of the application
+        ApplicationContext currentContext = SpringApplication
+                .run(LaptopshopApplication.class, args);
+    }
 
 }
